f3458d317a0462118eb71315b9a9497f3afb0683
Marcus Barbu <mgb.barbu@gmail.com>
2021-12-27 21:12:09 -0800

    cargo fmt on tests

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -440,13 +440,13 @@ mod tests {
             .filter_level(log::LevelFilter::Debug)
             .is_test(true)
             .try_init();
-        
+
         Chip8::test_core()
     }
 
     fn test_exec(chip8: &mut Chip8, instr: Chip8Instr) {
         match chip8.execute(instr) {
-            Ok(_) => {},
+            Ok(_) => {}
             Err(e) => {
                 error!("Failed to execute {:?} with err: {:?}", instr, e);
                 return;
@@ -471,7 +471,7 @@ mod tests {
     fn test_jump() {
         let mut chip8 = test_init();
         let addr: u16 = 0x234;
-        let jump_instr: Chip8Instr = Chip8Instr::Jump(Chip8LongImmOp{imm: addr});
+        let jump_instr: Chip8Instr = Chip8Instr::Jump(Chip8LongImmOp { imm: addr });
 
         test_exec(&mut chip8, jump_instr);
 
@@ -484,12 +484,16 @@ mod tests {
         let orig_pc: u16 = 12;
         let mut chip8 = test_init();
         chip8.regs.pc = orig_pc;
-        let call_instr: Chip8Instr = Chip8Instr::Call(Chip8LongImmOp{imm: addr});
+        let call_instr: Chip8Instr = Chip8Instr::Call(Chip8LongImmOp { imm: addr });
         test_exec(&mut chip8, call_instr);
 
         assert_eq!(chip8.regs.pc, addr, "Jump to method");
         assert_eq!(chip8.stack.len(), 1, "one item on stack _only_");
-        assert_eq!(*chip8.stack.get(0).unwrap(), orig_pc+2, "item on stack is original PC (incremented)");
+        assert_eq!(
+            *chip8.stack.get(0).unwrap(),
+            orig_pc + 2,
+            "item on stack is original PC (incremented)"
+        );
     }
 
     #[test]
@@ -499,17 +503,17 @@ mod tests {
         let mut chip8 = test_init();
         let mut pc: u16 = chip8.regs.pc;
 
-        chip8.set_reg(reg, val+1).unwrap();
+        chip8.set_reg(reg, val + 1).unwrap();
 
-        let skip_imm_eq_instr: Chip8Instr = Chip8Instr::SkipImmEq(Chip8SingleRegImmOp{ reg, imm: val });
+        let skip_imm_eq_instr: Chip8Instr =
+            Chip8Instr::SkipImmEq(Chip8SingleRegImmOp { reg, imm: val });
         test_exec(&mut chip8, skip_imm_eq_instr);
-        assert_eq!(chip8.regs.pc, pc+2, "Negative test"); // should NOT skip.
+        assert_eq!(chip8.regs.pc, pc + 2, "Negative test"); // should NOT skip.
 
         pc = chip8.regs.pc;
         chip8.set_reg(reg, val).unwrap();
         test_exec(&mut chip8, skip_imm_eq_instr);
-        assert_eq!(chip8.regs.pc, pc+4, "Positive Test"); // SHOULD skip
-
+        assert_eq!(chip8.regs.pc, pc + 4, "Positive Test"); // SHOULD skip
     }
 
     #[test]
@@ -519,16 +523,17 @@ mod tests {
         let mut chip8 = test_init();
         let mut pc: u16 = chip8.regs.pc;
 
-        chip8.set_reg(reg, val+1).unwrap();
+        chip8.set_reg(reg, val + 1).unwrap();
 
-        let skip_imm_eq_instr: Chip8Instr = Chip8Instr::SkipImmNe(Chip8SingleRegImmOp{ reg, imm: val });
+        let skip_imm_eq_instr: Chip8Instr =
+            Chip8Instr::SkipImmNe(Chip8SingleRegImmOp { reg, imm: val });
         test_exec(&mut chip8, skip_imm_eq_instr);
-        assert_eq!(chip8.regs.pc, pc+4, "Positive test"); // SHOULD skip
+        assert_eq!(chip8.regs.pc, pc + 4, "Positive test"); // SHOULD skip
 
         pc = chip8.regs.pc;
         chip8.set_reg(0, val).unwrap();
         test_exec(&mut chip8, skip_imm_eq_instr);
-        assert_eq!(chip8.regs.pc, pc+2, "Negative Test"); // should NOT skip.
+        assert_eq!(chip8.regs.pc, pc + 2, "Negative Test"); // should NOT skip.
     }
 
     #[test]
@@ -541,14 +546,15 @@ mod tests {
 
         chip8.set_reg(reg_a, val).unwrap();
         chip8.set_reg(reg_b, val).unwrap();
-        let skip_reg_eq_instr: Chip8Instr = Chip8Instr::SkipRegEq(Chip8DoubleRegOp{ a: reg_a, b: reg_b });
+        let skip_reg_eq_instr: Chip8Instr =
+            Chip8Instr::SkipRegEq(Chip8DoubleRegOp { a: reg_a, b: reg_b });
         test_exec(&mut chip8, skip_reg_eq_instr);
-        assert_eq!(chip8.regs.pc, pc+4, "Positive test"); // SHOULD skip
+        assert_eq!(chip8.regs.pc, pc + 4, "Positive test"); // SHOULD skip
 
         pc = chip8.regs.pc;
-        chip8.set_reg(reg_b, val+1).unwrap();
+        chip8.set_reg(reg_b, val + 1).unwrap();
         test_exec(&mut chip8, skip_reg_eq_instr);
-        assert_eq!(chip8.regs.pc, pc+2, "Negative test"); // should NOT skip.
+        assert_eq!(chip8.regs.pc, pc + 2, "Negative test"); // should NOT skip.
     }
 
     #[test]
@@ -557,9 +563,10 @@ mod tests {
         let reg: u8 = 0;
         let mut chip8 = test_init();
 
-        let set_reg_instr: Chip8Instr = Chip8Instr::RegAssign(Chip8SingleRegImmOp{ reg, imm: val });
+        let set_reg_instr: Chip8Instr =
+            Chip8Instr::RegAssign(Chip8SingleRegImmOp { reg, imm: val });
         test_exec(&mut chip8, set_reg_instr);
-        
+
         let res: u8 = chip8.get_reg(reg).unwrap();
         assert_eq!(res, val);
     }
@@ -572,10 +579,9 @@ mod tests {
         let mut chip8 = test_init();
 
         chip8.set_reg(reg, val).unwrap();
-        let incr_reg_instr: Chip8Instr = Chip8Instr::RegIncr(Chip8SingleRegImmOp{ reg, imm: inc });
+        let incr_reg_instr: Chip8Instr = Chip8Instr::RegIncr(Chip8SingleRegImmOp { reg, imm: inc });
         test_exec(&mut chip8, incr_reg_instr);
         let res: u8 = chip8.get_reg(reg).unwrap();
         assert_eq!(res, val + inc);
     }
-
 }
